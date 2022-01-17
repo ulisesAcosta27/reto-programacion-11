956d2f337b07d757784751bbdec6da70bd4c6cb2
ulises-david <ulisesdavidacosta27@gmail.com>
2022-01-17 16:53:58 -0300

    trabajo casi terminado

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const cargarContenido = async () => {
+document.getElementById('mainForm').addEventListener('submit', async e => {
+    e.preventDefault()
     try {
-        const respuesta = await fetch('https://api.mercadolibre.com/sites/MLA/search?q=iphone-11');
-        console.log(respuesta)
+        let elementoBuscado = e.target.elementoBuscado.value;
+        const respuesta = await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${elementoBuscado}`);
         const datos = await respuesta.json();
         console.log(datos.results)
         datos.results.forEach(telefono => {
             const creacionCard = document.createElement('div');
-        creacionCard.className = 'card_container';
+            creacionCard.className = 'card_container';
         creacionCard.innerHTML = `
         <div class="card_img">
             <img src="${telefono.thumbnail}" alt="">
@@ -25,7 +26,8 @@ const cargarContenido = async () => {
     });
 } catch (error) {
     console.log(error)
-    }
 }
+})
 
-cargarContenido()
+
+
